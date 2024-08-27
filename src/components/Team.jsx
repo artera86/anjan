94c1919a62f168b20ea030e033ad0d2ac2076242
artera86 <135210425+artera86@users.noreply.github.com>
2024-08-27 15:33:05 +0000

    .

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -10,22 +10,6 @@ export const Team = (props) => {
             For a comprehensive list of PlayStation games, head <a href="https://store.playstation.com/en-gb/pages/browse">here</a>!
           </p>
         </div>
-        <div id="row">
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
-                  <div className="thumbnail">
-                    {" "}
-                    <img src={d.img} alt="..." className="team-img" />
-                    <div className="caption">
-                      <h4>{d.name}</h4>
-                      <p>{d.job}</p>
-                    </div>
-                  </div>
-                </div>
-              ))
-            : "loading"}
-        </div>
       </div>
     </div>
   );
